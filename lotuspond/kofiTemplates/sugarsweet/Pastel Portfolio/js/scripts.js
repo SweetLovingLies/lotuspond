94743d688bf6bf4c55e93dda76da66d5874c8e59
Selkiefawn <136149232+Selkiefawn@users.noreply.github.com>
2025-07-12 17:09:02 -0400

    Addition of new Daylight Directory Demo, and clean up of the Pastel Portfolio Demo

--- lotuspond/kofiTemplates/sugarsweet/Pastel Portfolio/js/scripts.js
+++ lotuspond/kofiTemplates/sugarsweet/Pastel Portfolio/js/scripts.js
@@ -1,10 +1,3 @@
-
-/* 🌸 ----------------------------------------------------------------------------------
-    ~ You can mainly ignore this file (unless you want to try your hand at some fun stuff!), but if you're curious what it does:
-    ~ The hoverlinks are the links to your social media, at the bottom! This code is what allows the background  of the social media icon to change when you hover over the button. It's purely cosmetic, but it's fun!
-
------------------------------------------------------------------------------------ 🌸 */
-
 Fancybox.bind("[data-fancybox]", {}); // Lightbox setup!
 
 const hoverLinks = document.querySelectorAll('.listLink');
